9bb13ccc63472cb731d539ee20701b0aa3dd009c
Melisa Quesada <melisa.quc@gmail.com>
2019-07-08 23:19:47 -0500

    adds express and mailgun functions

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+var api_key = process.env.API_KEY;
+var domain = process.env.MY_DOMAIN;
+var mail = process.env.MY_MAIL;
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+app.post('/sendEmail', function(req, res) {
+    res.send('POST request');
+
+    var data = {
+        from: 'Excited User <me@samples.mailgun.org>',
+        to: 'mel.test9@gmail.',
+        subject: 'Hello',
+        text: 'Testing some Mailgun awesomeness!'
+      };
+
+      mailgun.messages().send(data, function (err, body) {
+        if(!error) {
+            console.log(body);
+        } else {
+            console.log(err);
+        }
+      });
+});
